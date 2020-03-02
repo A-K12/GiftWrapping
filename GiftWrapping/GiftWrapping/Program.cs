2f0d50629882fd7d55ccb0f1e6a65b35c6c18cbb
Alex <xalexseyx5@gmail.com>
2020-03-02 13:27:09 +0500

    Initial commit

---
+++ GiftWrapping/GiftWrapping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GiftWrapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
