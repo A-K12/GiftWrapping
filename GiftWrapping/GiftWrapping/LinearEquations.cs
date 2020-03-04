fb1fa7265aa37567eea7e78060f3b4ea2351fdaf
Alex <xalexseyx5@gmail.com>
2020-03-04 21:43:57 +0500

    Change Gausses method

--- GiftWrapping/GiftWrapping/LinearEquations.cs
+++ GiftWrapping/GiftWrapping/LinearEquations.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using GiftWrapping.Structures;
 
@@ -13,74 +15,79 @@ namespace GiftWrapping
 
         public static Vector GaussWithChoiceSolveSystem(Matrix matrix, Vector vector)
         {
-            var row = new bool[matrix.Rows];
-            var column = new bool[matrix.Cols];
-            var dimension = matrix.Cols;
-            var columnPosition = new int[dimension];
+            var row = GetIndexArray(matrix.Rows);
+            var column = GetIndexArray(matrix.Cols);
+            var dim = matrix.Cols;
             var tempMatrix = (double[,])matrix;
             var tempVector = (double[]) vector;
-            for (int i = 0; i < dimension; i++)
-            {
-                columnPosition[i] = i;
-            }
+           
 
-            for (int i = 0; i < matrix.Rows; i++)
+            for (int i = 0; i < row.Length; i++)
             {
                 var majorColumn = i;
                 var majorRow = i;
-                for (int j = i; j < matrix.Rows; j++)
-                    for (int k = 0; k < dimension; k++)
-                        if (Math.Abs(tempMatrix[j,k]) > Math.Abs(tempMatrix[majorRow,majorColumn]))
+                for (int j = i; j < row.Length; j++)
+                {
+                    for (int k = 0; k < column.Length; k++)
+                    {
+                        if (Math.Abs(tempMatrix[row[j], column[k]]) > 
+                            Math.Abs(tempMatrix[row[majorRow], column[majorColumn]]))
                         {
                             majorColumn = k;
                             majorRow = j;
                         }
-
-                Console.Out.WriteLine("Максимальный вектор = {0}", tempMatrix[majorRow,majorColumn]);
+                    }
+                }
+                Show(tempMatrix, tempVector, "Начальная матрица", row, column);
+                Console.Out.WriteLine("Максимальный вектор = {0}", tempMatrix[row[majorRow],column[majorColumn]]);
                 Console.Out.WriteLine(" Индексы = Строка {0}, Колонка{1}", majorRow,majorColumn);
 
-                SwapRows(ref tempMatrix, i, majorRow);
-                SwapCoordinates(ref tempVector, i, majorRow);
 
-                SwapColumn(ref tempMatrix, i, majorColumn);
-                SwapCoordinates(ref columnPosition,i, majorColumn);
+               
+                SwapCoordinates(ref row, i, majorRow);
+                Show(tempMatrix, tempVector, "После свайпа строк", row, column);
+                
+                SwapCoordinates(ref column,i, majorColumn);
+                Show(tempMatrix, tempVector, "После свайпа столбцов", row, column);
 
-                Show(tempMatrix, tempVector, "После свайпа");
-                var index = i;
-                for (int j = i+1; j < matrix.Rows; j++)
+                Show(tempMatrix, tempVector, "После свайпа", row, column);
+                var index =i;
+                for (int j = i + 1; j < matrix.Rows; j++)
                 {
                     Console.Out.WriteLine("Line = {0}", j);
-                    var multiplier = tempMatrix[j,i] / tempMatrix[index,i];
+                    var multiplier = tempMatrix[row[j], column[i]] / tempMatrix[row[index],column[i]];
                     Console.Out.WriteLine("multiplier = {0}", multiplier);
-                    tempMatrix[j,i] = 0;
-                    for (int k = i+1; k < dimension; k++)
+                    tempMatrix[row[j], column[i]] = 0;
+                    for (int k = i + 1; k < dim; k++)
                     {
-                        tempMatrix[j,k] -= multiplier * tempMatrix[index,k];
+                        tempMatrix[row[j], column[k]] -= multiplier * tempMatrix[row[index],column[k]];
                     }
-                    tempVector[j] -= multiplier * tempVector[index];
-                    Show(tempMatrix, tempVector, "После умножения");
+
+                    tempVector[row[j]] -= multiplier * tempVector[row[index]];
+                    Show(tempMatrix, tempVector, "После умножения", row, column);
+                    
                 }
             }
-            var answer = new double[dimension];
-            var indexLastX = Array.IndexOf(columnPosition, dimension - 1);
+            var answer = new double[dim];
+            var indexLastX = Array.IndexOf(column, dim - 1);
             answer[indexLastX] = 1.0;  
             for (int i = matrix.Rows-1; i >= 0; i--)
             {   
-                var x = tempVector[i];
-                for (int j = dimension - 1; j > i; --j)
+                var x = tempVector[row[i]];
+                for (int j = dim - 1; j > i; --j)
                 {
-                    x -= tempMatrix[i,j] * answer[j];
+                    x -= tempMatrix[row[i],column[j]] * answer[j];
                 }
-                if (Math.Abs(tempMatrix[i,i]) > _eps)
+                if (Math.Abs(tempMatrix[row[i],column[i]]) > _eps)
                 {
-                    x /= tempMatrix[i,i];
+                    x /= tempMatrix[row[i],column[i]];
                 }
                 answer[i] = x;
             }
-            var sortAnswer = new double[dimension];
-            for (int i = 0; i < dimension; ++i)
+            var sortAnswer = new double[dim];
+            for (int i = 0; i < dim; ++i)
             {
-                var j = (int)columnPosition[i];
+                var j = (int)column[i];
                 sortAnswer[j] = answer[i];
             }
 
@@ -88,19 +95,30 @@ namespace GiftWrapping
             //return new Point(4);
         }
 
+        private static int[] GetIndexArray(int lenght)
+        {
+            var array = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = i;
+            }
 
-        private static void Show(double[,] matrix, double[] vector, string text = "Матрица")
+            return array;
+        }
+
+
+        private static void Show(double[,] matrix, double[] vector, string text, int[] row, int[] column)
         {
             Console.Out.WriteLine(text);
             Console.Out.WriteLine("---------------");
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < row.Length; i++)
             {
                 
-                for (int j = 0; j < vector.Length; j++)
+                for (int j = 0; j < column.Length; j++)
                 {
-                    Console.Out.Write("{0:0.###} ", matrix[i,j]);
+                    Console.Out.Write("{0:0.###} ", matrix[row[i],column[j]]);
                 }
-                Console.Out.Write(" | "+ vector[i]);
+                Console.Out.Write(" | "+ vector[row[i]]);
                 Console.Out.WriteLine("");
             }
         }
@@ -134,11 +152,11 @@ namespace GiftWrapping
             vector[indexB] = temp;
         }
 
-        private static void SwapCoordinates(ref int[] tempMatrix, int indexA, int indexB)
+        private static void SwapCoordinates(ref int[] vector, int indexA, int indexB)
         {
-            var temp = tempMatrix[indexA];
-            tempMatrix[indexA] = tempMatrix[indexB];
-            tempMatrix[indexB] = temp;
+            var temp = vector[indexA];
+            vector[indexA] = vector[indexB];
+            vector[indexB] = temp;
         }
     }
 }
--- GiftWrapping/GiftWrapping/Program.cs
+++ GiftWrapping/GiftWrapping/Program.cs
@@ -8,33 +8,33 @@ namespace GiftWrapping
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var points = new Point[2];
-            points[0] = new Point(1, 2, 3);
-            points[1] = new Point( 4,5,6 );
+            //var points = new Point[2];
+            //points[0] = new Point(1, 2, 3);
+            //points[1] = new Point( 4,5,6 );
 
-            var vector2 = new Point(0, 0, 0);
+            //var vector2 = new Point(0, 0, 0);
 
-            var points1 = new Point[3];
-            points1[0] = new Point(1, 2, 3);
-            points1[1] = new Point(3, 5, 7);
-            points1[2] = new Point(1, 3, 4);
+            //var points1 = new Point[3];
+            //points1[0] = new Point(1, 2, 3);
+            //points1[1] = new Point(3, 5, 7);
+            //points1[2] = new Point(1, 3, 4);
 
-            var vector = new Point(3, 0, 1);
+            //var vector = new Point(3, 0, 1);
 
 
-            var points4 = new Point[4]
-            {
-                new Point(1, 1, 0),
-                new Point(6, 1, 0),
-                new Point(3, 3, 0),
-                new Point(3, 3, 6)
-            };
+            //var points4 = new Point[4]
+            //{
+            //    new Point(1, 1, 0),
+            //    new Point(6, 1, 0),
+            //    new Point(3, 3, 0),
+            //    new Point(3, 3, 6)
+            //};
 
             var points6 = new double[3, 3]
             {
                 {1, 2, 3},
-                {3, 5, 7},
-                {1, 3, 4 }
+                {1, 3, 4 },
+                {3, 5, 7}
             };
             var matrix = new Matrix(points6);
             var vector3 = new Vector(new double[3] { 3, 0, 1 });
@@ -47,10 +47,6 @@ namespace GiftWrapping
             var arra = LinearEquations.GaussWithChoiceSolveSystem(matrix, vector3);
           // var arra = faceFinder.FindFacePoints(points4);
             //var arra = LinearEquations.Calculate(points);
-            for (int i = 0; i < arra.Length; i++)
-            {
-                //Console.Out.Write(arra[i] + " ");
-            }
             Console.ReadLine();
         }
     }
